b55fc3ba01480d73b32bd862c927260c112cf26c
tanghc <thc8775@163.com>
2021-01-18 15:56:11 +0800

    UPDATE

--- server/server-common/src/main/java/torna/common/context/UserContext.java
+++ server/server-common/src/main/java/torna/common/context/UserContext.java
@@ -51,7 +51,7 @@ public class UserContext {
         try {
             return getUser(token);
         } catch (ErrorTokenException e) {
-            return null;
+            throw new LoginFailureException();
         }
     }
 
