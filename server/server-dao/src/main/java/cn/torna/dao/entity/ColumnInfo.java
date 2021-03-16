4bd9bfdff0f86f61bdecee24cd9c193ecf0036a1
tanghc <thc8775@163.com>
2021-03-16 18:11:50 +0800

    代码优化

--- server/server-dao/src/main/java/cn/torna/dao/entity/ColumnInfo.java
+++ server/server-dao/src/main/java/cn/torna/dao/entity/ColumnInfo.java
@@ -1,27 +1,19 @@
 package cn.torna.dao.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author tanghc
  */
+@Getter
+@Setter
 public class ColumnInfo {
     /** 字段名 */
     private String name;
     /** 字段类型 */
     private String type;
 
-    public String getName() {
-        return name;
-    }
+    private String length;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
 }
