611e352bbf2d13370895704cf8924fa761a38ca5
tanghc <thc8775@163.com>
2021-03-17 17:14:07 +0800

    1.0.0

---
+++ server/server-dao/src/main/java/cn/torna/dao/mapper/DocSnapshotMapper.java
@@ -0,0 +1,20 @@
+package cn.torna.dao.mapper;
+
+import com.gitee.fastmybatis.core.mapper.CrudMapper;
+import org.apache.ibatis.annotations.ResultType;
+import org.apache.ibatis.annotations.Select;
+import cn.torna.dao.entity.DocSnapshot;
+
+import java.util.List;
+
+/**
+ * @author tanghc
+ */
+public interface DocSnapshotMapper extends CrudMapper<DocSnapshot, Long> {
+
+    @Select("SELECT id, modifier_name modifierName, modifier_time modifierTime " +
+            "FROM doc_snapshot WHERE doc_id=#{docId} ORDER BY id DESC LIMIT 100")
+    @ResultType(DocSnapshot.class)
+    List<DocSnapshot> listDocSnapshotBaseInfo(long docId);
+	
+}
