73ab4d9551a406813eb560e2c92f234bc3807710
tanghc <thc8775@163.com>
2021-04-19 19:41:00 +0800

    1.4.1

--- server/boot/src/main/java/cn/torna/TornaApplication.java
+++ server/boot/src/main/java/cn/torna/TornaApplication.java
@@ -2,16 +2,23 @@ package cn.torna;
 
 import cn.torna.service.UpgradeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 
-@SpringBootApplication(scanBasePackages = {"cn.torna", "com.anji.captcha"})
+import java.util.TimeZone;
+
+@SpringBootApplication(scanBasePackages = {"cn.torna"})
 public class TornaApplication implements ApplicationRunner {
 
     @Autowired
     private UpgradeService upgradeService;
+
+    @Value("${torna.time-zone:}")
+    private String timeZone;
 
     public static void main(String[] args) {
         SpringApplication.run(TornaApplication.class, args);
@@ -20,6 +27,13 @@ public class TornaApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        initTimeZone();
         upgradeService.upgrade();
+    }
+
+    private void initTimeZone() {
+        if (StringUtils.hasText(timeZone)) {
+            TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+        }
     }
 }
