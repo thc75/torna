edf88b5ce42fa3cfd47a73e1840f6c983a8dc81c
tanghc <thc8775@163.com>
2021-04-19 19:57:22 +0800

    1.4.1

--- server/boot/src/main/java/cn/torna/TornaApplication.java
+++ server/boot/src/main/java/cn/torna/TornaApplication.java
@@ -2,23 +2,16 @@ package cn.torna;
 
 import cn.torna.service.UpgradeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.StringUtils;
-
-import java.util.TimeZone;
 
 @SpringBootApplication(scanBasePackages = {"cn.torna"})
 public class TornaApplication implements ApplicationRunner {
 
     @Autowired
     private UpgradeService upgradeService;
-
-    @Value("${torna.time-zone:}")
-    private String timeZone;
 
     public static void main(String[] args) {
         SpringApplication.run(TornaApplication.class, args);
@@ -27,14 +20,7 @@ public class TornaApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        initTimeZone();
         upgradeService.upgrade();
     }
 
-    private void initTimeZone() {
-        if (StringUtils.hasText(timeZone)) {
-            System.out.println("时区设置:" + timeZone);
-            TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
-        }
-    }
 }
