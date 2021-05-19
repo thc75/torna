0a7a811f77a91a99a11226197ab1da172677a4cd
tanghc <thc8775@163.com>
2021-05-19 18:39:34 +0800

    http

--- server/server-web/src/main/java/cn/torna/web/interceptor/LoginInterceptor.java
+++ server/server-web/src/main/java/cn/torna/web/interceptor/LoginInterceptor.java
@@ -4,6 +4,7 @@ import cn.torna.common.bean.User;
 import cn.torna.common.context.UserContext;
 import cn.torna.common.enums.UserStatusEnum;
 import cn.torna.common.exception.SetPasswordException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -16,10 +17,12 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * @author tanghc
  */
+@Slf4j
 public class LoginInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("getRequestURI:{}, getRequestURL:{}, contentPath:{}", request.getRequestURI(), request.getRequestURL(), request.getContextPath());
         HandlerMethod handlerMethod = (HandlerMethod) handler;
         NoLogin noLogin = handlerMethod.getMethodAnnotation(NoLogin.class);
         if (noLogin != null) {
