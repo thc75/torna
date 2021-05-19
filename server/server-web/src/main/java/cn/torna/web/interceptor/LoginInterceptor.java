0d4503be46e707317bb6e5eb8743fad3c3c52f2d
tanghc <thc8775@163.com>
2021-05-19 18:56:20 +0800

    http

--- server/server-web/src/main/java/cn/torna/web/interceptor/LoginInterceptor.java
+++ server/server-web/src/main/java/cn/torna/web/interceptor/LoginInterceptor.java
@@ -1,15 +1,14 @@
 package cn.torna.web.interceptor;
 
+import cn.torna.common.annotation.NoLogin;
 import cn.torna.common.bean.User;
 import cn.torna.common.context.UserContext;
 import cn.torna.common.enums.UserStatusEnum;
+import cn.torna.common.exception.LoginFailureException;
 import cn.torna.common.exception.SetPasswordException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-import cn.torna.common.annotation.NoLogin;
-import cn.torna.common.exception.LoginFailureException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,12 +16,10 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * @author tanghc
  */
-@Slf4j
 public class LoginInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        log.info("getRequestURI:{}, getRequestURL:{}, contentPath:{}", request.getRequestURI(), request.getRequestURL(), request.getContextPath());
         HandlerMethod handlerMethod = (HandlerMethod) handler;
         NoLogin noLogin = handlerMethod.getMethodAnnotation(NoLogin.class);
         if (noLogin != null) {
