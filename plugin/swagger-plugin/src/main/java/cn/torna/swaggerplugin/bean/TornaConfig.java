6a5faa63d6430bbe46d27331437ad3750d15ec5d
tanghc <thc8775@163.com>
2021-12-03 09:38:55 +0800

    feat:插件配置可指定object类型的类

--- plugin/swagger-plugin/src/main/java/cn/torna/swaggerplugin/bean/TornaConfig.java
+++ plugin/swagger-plugin/src/main/java/cn/torna/swaggerplugin/bean/TornaConfig.java
@@ -13,45 +13,87 @@ public class TornaConfig {
 
     private String configFile;
 
-    /** 开启推送 */
+    /**
+     * 开启推送
+     */
     private Boolean enable = false;
-    /** 扫描package，多个用";"隔开。不指定扫描全部 */
+    /**
+     * 扫描package，多个用";"隔开。不指定扫描全部
+     */
     private String basePackage;
-    /** 指定扫描的API，可以是某个controller，也可以是某个接口方法 */
+    /**
+     * 指定扫描的API，可以是某个controller，也可以是某个接口方法
+     */
     private List<String> scanApis;
-    /** 推送URL */
+    /**
+     * 推送URL
+     */
     private String url;
-    /** appKey */
+    /**
+     * appKey
+     */
     private String appKey;
-    /** secret */
+    /**
+     * secret
+     */
     private String secret;
-    /** 模块token */
+    /**
+     * 模块token
+     */
     private String token;
 
-    /** 调试环境，格式：环境名称,调试路径，多个用"|"隔开 */
+    /**
+     * 调试环境，格式：环境名称,调试路径，多个用"|"隔开
+     */
     private String debugEnv;
-    /** 推送人 */
+    /**
+     * 推送人
+     */
     private String author;
-    /** 打开调试:true/false */
+    /**
+     * 打开调试:true/false
+     */
     private Boolean debug = true;
-    /** 调试输出json格式化 */
+    /**
+     * 调试输出json格式化
+     */
     private Boolean debugPrintFormat = false;
-    /** 接口多个method只显示 */
+    /**
+     * 接口多个method只显示
+     */
     private String methodWhenMulti = "GET";
-    /** 具有body体的方法 */
+    /**
+     * 具有body体的方法
+     */
     private String hasBodyMethods = "POST,PUT,DELETE";
-    /** 是否替换文档，true：替换，false：不替换（追加）。默认：true */
+    /**
+     * 是否替换文档，true：替换，false：不替换（追加）。默认：true
+     */
     private Boolean isReplace = true;
 
-    /** 全局的contentType */
+    /**
+     * 全局的contentType
+     */
     private String globalContentType = "application/x-www-form-urlencoded";
-    /** 默认的http method */
+    /**
+     * 默认的http method
+     */
     private String defaultHttpMethod = "POST";
 
-    /** 第三方jar */
+    /**
+     * 第三方jar
+     */
     private JSONObject jarClass;
 
-    /** 定义错误码 */
+    /**
+     * 定义错误码
+     */
     private List<CodeInfo> codes;
+
+    /**
+     * 只能是object类型对象
+     * 如：com.alibaba.fastjson.JSONObject
+     */
+    private List<String> objectClassList;
 
 }
