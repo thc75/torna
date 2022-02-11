6a56028864ff814ddecc2b11438f6d3b4a2c4d9c
tanghc <thc8775@163.com>
2022-02-11 13:41:56 +0800

    1.13.1

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ set dist_dir=dist
 
 set app_name=torna
 
-set version="1.13.0"
+set version="1.13.1"
 
 set build_folder=%app_name%-%version%
 
--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ dist_dir="dist"
 # 执行文件名称
 app_name="torna"
 
-version="1.13.0"
+version="1.13.1"
 
 build_folder="${app_name}-${version}"
 
