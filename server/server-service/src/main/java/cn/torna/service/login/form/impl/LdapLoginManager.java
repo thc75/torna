e38e1590aabc7fb228781f05c596336e87698df5
tanghc <thc8775@163.com>
2022-03-13 13:06:34 +0800

    优化LDAP登录

--- server/server-service/src/main/java/cn/torna/service/login/form/impl/LdapLoginManager.java
+++ server/server-service/src/main/java/cn/torna/service/login/form/impl/LdapLoginManager.java
@@ -6,6 +6,7 @@ import cn.torna.service.login.form.LoginForm;
 import cn.torna.service.login.form.LoginResult;
 import cn.torna.service.login.form.ThirdPartyLoginManager;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ldap.core.LdapTemplate;
@@ -13,12 +14,23 @@ import org.springframework.ldap.query.LdapQueryBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
+import java.util.Hashtable;
+
 /**
  * LDAP登录
  */
 @Slf4j
 @Service
-public class LdapLoginManager implements ThirdPartyLoginManager {
+public class LdapLoginManager implements ThirdPartyLoginManager, InitializingBean {
+
 
     @Autowired
     private LdapTemplate ldapTemplate;
@@ -26,12 +38,36 @@ public class LdapLoginManager implements ThirdPartyLoginManager {
     @Value("${torna.email-domain:}")
     private String emailDomain;
 
+    @Value("${torna.ldap.custom-search-filter:(&(objectClass=inetOrgPerson)(uid=%s))}")
+    private String ldapSearchFilter;
+
+    @Value("${torna.ladp.custom-url:${torna.ldap.url:}/${torna.ldap.base:}}")
+    private String customUrl;
+
+    @Value("${torna.ldap.custom-base-dn:${torna.ldap.base:}}")
+    private String customBaseDn;
+
+    @Value("${torna.ldap.custom-username:${torna.ldap.username:}}")
+    private String customUsername;
+
+    @Value("${torna.ldap.custom-password:${torna.ldap.password:}}")
+    private String customPassword;
+
+    @Value("${torna.ldap.custom-context-factory:com.sun.jndi.ldap.LdapCtxFactory}")
+    private String factory;
+
+
+    private LdapContext ldapContext;
 
     @Override
     public LoginResult login(LoginForm loginForm) throws Exception {
         String username = loginForm.getUsername();
         String password = loginForm.getPassword();
-        LdapUser ldapUser = ldapAuth(username, password);
+        LdapUser ldapUser = authNormal(username, password);
+        // 正常流程登录失败，再使用spring封装的登录
+        if (ldapUser == null) {
+            ldapUser = ldapAuth(username, password);
+        }
         if (ldapUser == null) {
             throw new BizException("登录失败");
         }
@@ -54,7 +90,8 @@ public class LdapLoginManager implements ThirdPartyLoginManager {
 
     /**
      * LDAP登录
-     * @param uid 用户名
+     *
+     * @param uid      用户名
      * @param password 密码
      * @return 登录成功返回登录信息，否则返回null
      */
@@ -68,6 +105,74 @@ public class LdapLoginManager implements ThirdPartyLoginManager {
         } catch (Exception e) {
             log.error("LDAP登录失败，username={}", uid, e);
             return null;
+        }
+    }
+
+    public LdapUser authNormal(String uid, String password) {
+        if (ldapContext == null) {
+            return null;
+        }
+        SearchResult searchResult = getUserInfo(uid);
+        if (searchResult == null) {
+            return null;
+        }
+        String userDN = searchResult.getName() + "," + customBaseDn;
+        try {
+            ldapContext.addToEnvironment(Context.SECURITY_PRINCIPAL, userDN);
+            ldapContext.addToEnvironment(Context.SECURITY_CREDENTIALS, password);
+            ldapContext.reconnect(null);
+            // 成功表示用户名密码正确
+            return buildLdapUser(searchResult);
+        } catch (Exception e) {
+            log.error("LDAP认证失败, uid={}", uid, e);
+            return null;
+        }
+    }
+
+    private LdapUser buildLdapUser(SearchResult searchResult) throws NamingException {
+        Attributes attributes = searchResult.getAttributes();
+        String uid = String.valueOf(attributes.get("uid").get());
+        String displayName = String.valueOf(attributes.get("displayName").get());
+        String mail = String.valueOf(attributes.get("mail").get());
+        LdapUser ldapUser = new LdapUser();
+        ldapUser.setUid(uid);
+        ldapUser.setDisplayName(displayName);
+        ldapUser.setMail(mail);
+        log.debug("LDAP自定义配置登录成功，ldapUser={}", ldapUser);
+        return ldapUser;
+    }
+
+    private SearchResult getUserInfo(String uid) {
+        try {
+            SearchControls controls = new SearchControls();
+            controls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+            NamingEnumeration<SearchResult> results = ldapContext.search("", String.format(ldapSearchFilter, uid), controls);
+            if (results == null || !results.hasMoreElements()) {
+                return null;
+            }
+            return results.nextElement();
+        } catch (Exception e) {
+            log.error("查找用户{}出错", uid, e);
+            return null;
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (StringUtils.hasText(customBaseDn)) {
+            log.info("LDAP配置，url:{}, baseDN:{}, username:{}", customUrl, customBaseDn, customUsername);
+            Hashtable<String, String> env = new Hashtable<>();
+
+            env.put(Context.INITIAL_CONTEXT_FACTORY, factory);
+            // ldap://192.168.153.129:389/dc=contoso,dc=com
+            env.put(Context.PROVIDER_URL, customUrl);
+            env.put(Context.SECURITY_PRINCIPAL, customUsername);
+            env.put(Context.SECURITY_CREDENTIALS, customPassword);
+            try {
+                ldapContext = new InitialLdapContext(env, null);
+            } catch (Exception e) {
+                log.error("初始化LDAP失败", e);
+            }
         }
     }
 
