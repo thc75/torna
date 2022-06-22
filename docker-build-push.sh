9d333d945be6493a65142fd39de5c7678f51634e
tanghc <thc8775@163.com>
2022-06-22 15:32:56 +0800

    修改docker脚本

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -7,13 +7,8 @@ git pull
 
 sh release.sh
 
-echo "开始创建docker hub镜像"
-docker build -t tanghc2020/torna .
-
-sleep 1
-
-echo "开始创建docker hub镜像:${1}"
-docker build -t tanghc2020/torna:${1} .
+echo "开始创建docker hub镜像:latest"
+docker build -t tanghc2020/torna:latest .
 
 sleep 1
 
@@ -24,6 +19,11 @@ sleep 1
 
 # 如果有参数
 if [ -n "${1}" ];then
+  echo "开始创建docker hub镜像:${1}"
+  docker build -t tanghc2020/torna:${1} .
+
+  sleep 1
+
   echo "推送镜像到docker hub:${1}"
   docker push tanghc2020/torna:${1}
 fi
--- docker-build.sh
+++ docker-build.sh
@@ -6,8 +6,8 @@ git pull
 
 sh release.sh
 
-echo "开始创建docker hub镜像"
-docker build -t tanghc2020/torna .
+echo "开始创建docker hub镜像:latest"
+docker build -t tanghc2020/torna:latest .
 
 if [ -n "${1}" ];then
   echo "开始创建docker hub镜像:${1}"
