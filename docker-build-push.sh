79c1c475c6336acb10df5774f740dff107cbe3b3
tanghc <thc8719@163.com>
2023-06-29 11:40:56 +0800

    自定义镜像

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# 构建docker并推送到阿里云
 # 执行方式：sh docker-build-push.sh <版本号>
 # 如：sh docker-build-push.sh 1.15.2
 
@@ -8,22 +9,22 @@ git pull
 sh release.sh
 
 echo "开始创建docker hub镜像:latest"
-docker build -f Dockerfile.local -t tanghc2020/torna:latest .
+docker build -f Dockerfile.local -t registry.cn-hangzhou.aliyuncs.com/tanghc/torna:latest .
 
 sleep 1
 
-echo "推送镜像到docker hub:latest，执行命令：docker push tanghc2020/torna:latest"
-docker push tanghc2020/torna:latest
+echo "推送镜像到docker hub:latest，执行命令：docker push registry.cn-hangzhou.aliyuncs.com/tanghc/torna:latest"
+docker push registry.cn-hangzhou.aliyuncs.com/tanghc/torna:latest
 
 sleep 1
 
 # 如果有参数
 if [ -n "${1}" ];then
   echo "开始创建docker hub镜像:${1}"
-  docker build -f Dockerfile.local -t tanghc2020/torna:${1} .
+  docker build -f Dockerfile.local -t registry.cn-hangzhou.aliyuncs.com/tanghc/torna:${1} .
 
   sleep 1
 
-  echo "推送镜像到docker hub:${1}，执行命令：docker push tanghc2020/torna:${1}"
-  docker push tanghc2020/torna:${1}
+  echo "推送镜像到docker hub:${1}，执行命令：docker push registry.cn-hangzhou.aliyuncs.com/tanghc/torna:${1}"
+  docker push registry.cn-hangzhou.aliyuncs.com/tanghc/torna:${1}
 fi
