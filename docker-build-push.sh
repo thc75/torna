dba49c8a6029630af8104d7ee71ab4bf6d2949db
tanghc <thc8775@163.com>
2024-01-20 11:52:21 +0800

    1.26.0

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -8,24 +8,27 @@ git pull
 
 sh release.sh
 
+docker_host="registry.cn-hangzhou.aliyuncs.com"
+vpc_host="registry-vpc.cn-hangzhou.aliyuncs.com"
+
 echo "开始创建docker hub镜像:latest"
-docker build -f Dockerfile.local -t registry.cn-hangzhou.aliyuncs.com/tanghc/torna:latest .
+docker build -f Dockerfile.local -t ${docker_host}/tanghc/torna:latest .
 
 sleep 1
 
-echo "推送镜像到docker hub:latest，执行命令：docker push registry.cn-hangzhou.aliyuncs.com/tanghc/torna:latest"
-docker push registry.cn-hangzhou.aliyuncs.com/tanghc/torna:latest
+echo "推送镜像到docker hub:latest，执行命令：docker push ${vpc_host}/tanghc/torna:latest"
+docker push ${vpc_host}/tanghc/torna:latest
 
 sleep 1
 
 # 如果有参数
 if [ -n "${1}" ];then
 
-  echo "tag 镜像，命令：docker tag registry.cn-hangzhou.aliyuncs.com/tanghc/torna:latest registry.cn-hangzhou.aliyuncs.com/tanghc/torna:${1}"
-  docker tag registry.cn-hangzhou.aliyuncs.com/tanghc/torna:latest registry.cn-hangzhou.aliyuncs.com/tanghc/torna:${1}
+  echo "tag 镜像，命令：docker tag ${docker_host}/tanghc/torna:latest ${docker_host}/tanghc/torna:${1}"
+  docker tag ${vpc_host}/tanghc/torna:latest registry.cn-hangzhou.aliyuncs.com/tanghc/torna:${1}
 
   sleep 1
 
-  echo "推送镜像到docker hub:${1}，执行命令：docker push registry.cn-hangzhou.aliyuncs.com/tanghc/torna:${1}"
-  docker push registry.cn-hangzhou.aliyuncs.com/tanghc/torna:${1}
+  echo "推送镜像到docker hub:${1}，执行命令：docker push ${vpc_host}/tanghc/torna:${1}"
+  docker push ${vpc_host}/tanghc/torna:${1}
 fi
