80e9395c9417c64207ffdc26f4ef7f8d1241d2b9
tanghc <thc8775@163.com>
2024-01-20 16:38:52 +0800

    1.26.0

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -8,26 +8,26 @@ git pull
 
 sh release.sh
 
-docker_host="registry-vpc.cn-hangzhou.aliyuncs.com"
+docker_host="registry-vpc.cn-hangzhou.aliyuncs.com/tanghc"
 
 echo "开始创建docker hub镜像:latest"
-docker build -f Dockerfile.local -t ${docker_host}/tanghc/torna:latest .
+docker build -f Dockerfile.local -t ${docker_host}/torna:latest .
 
 sleep 1
 
-echo "推送镜像到docker hub:latest，执行命令：docker push ${docker_host}/tanghc/torna:latest"
-docker push ${docker_host}/tanghc/torna:latest
+echo "推送镜像到docker hub:latest，执行命令：docker push ${docker_host}/torna:latest"
+docker push ${docker_host}/torna:latest
 
 sleep 1
 
 # 如果有参数
 if [ -n "${1}" ];then
 
-  echo "tag 镜像，命令：docker tag ${docker_host}/tanghc/torna:latest ${docker_host}/tanghc/torna:${1}"
-  docker tag ${docker_host}/tanghc/torna:latest ${docker_host}/tanghc/torna:${1}
+  echo "tag 镜像，命令：docker tag ${docker_host}/torna:latest ${docker_host}/torna:${1}"
+  docker tag ${docker_host}/torna:latest ${docker_host}/torna:${1}
 
   sleep 1
 
-  echo "推送镜像到docker hub:${1}，执行命令：docker push ${docker_host}/tanghc/torna:${1}"
-  docker push ${docker_host}/tanghc/torna:${1}
+  echo "推送镜像到docker hub:${1}，执行命令：docker push ${docker_host}/torna:${1}"
+  docker push ${docker_host}/torna:${1}
 fi
