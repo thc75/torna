7cc2041cfa2cc8c29ff634d0803b74c62feb8e7d
muyao <3202302816@qq.com>
2024-12-31 20:10:09 +0800

    [慕瑶] 大接口的前端性能优化（请求参数界面和基本信息）

---
+++ front/src/views/doc/EditTable/input.vue
@@ -0,0 +1,52 @@
+<template>
+    <el-input v-model="value1" size="small"  @change="onChange" :placeholder="placeholder1" :size="size" :maxlength="maxlength || ''" :show-word-limit="showWordLimit"></el-input>
+</template>
+<script>
+export default {
+  name: 'Input',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    placeholder:{
+      type: String,
+      default: ''
+    },
+    size:{
+      type: String,
+      default: ''
+    },
+    maxlength:{
+      type: Number,
+      default: 0
+    },
+    showWordLimit:{
+      type: Boolean,
+      default: false
+    },
+  },
+  data() {
+    return {
+      value1: this.value,
+      placeholder1:this.placeholder,
+    }
+  },
+  watch: {
+    data(val) {
+        console.log(val)
+      this.value = val
+    }
+  },
+  methods: {
+    onChange() {
+        console.log(this.value1)
+        this.$emit('change',this.value1)
+    },
+  }
+}
+</script>
+ 
+<style scoped lang="scss">
+ 
+</style>
---
+++ front/src/views/doc/EditTable/switch.vue
@@ -0,0 +1,51 @@
+<template>
+    <el-switch
+      v-model="value1"
+      :active-color="activeColor"
+      :active-value="activeVlaue"
+      :inactive-value="inactiveValue"
+      @change="onChange" 
+    />
+</template>
+<script>
+export default {
+  name: 'Switch1',
+  props: {
+    value: {
+      type: Number,
+      default: null
+    },
+    activeColor:{
+      type: String,
+      default: ''
+    },
+    activeVlaue:{
+      type: Number,
+      default: 1
+    },
+    inactiveValue:{
+      type: Number,
+      default: 0
+    },
+  },
+  data() {
+    return {
+      value1: this.value,
+    }
+  },
+  watch: {
+    data(val) {
+      this.value = val
+    }
+  },
+  methods: {
+    onChange() {
+      this.$emit('change',this.value1)
+    },
+  }
+}
+</script>
+ 
+<style scoped lang="scss">
+ 
+</style>
